8b1ad04ffb6bd7861d639e354a0f8b63cdbcfb03
ellesmith88 <e.s.smith@hotmail.co.uk>
2020-01-09 12:06:45 +0000

    removed modified date

--- datamad2/models.py
+++ datamad2/models.py
@@ -85,7 +85,7 @@ class Grant(models.Model):
 class ImportedGrant(models.Model):
     # ordering by creation date
     class Meta:
-        ordering = ['-modified_date']
+        ordering = ['-creation_date']
 
     # Title	Siebel	Name of the grant			PROJECT_TITLE
     title = models.CharField(max_length=1024, default='')
@@ -97,7 +97,7 @@ class ImportedGrant(models.Model):
     # creation_date = models.DateTimeField(auto_now_add=True)
     creation_date = models.DateTimeField(editable=False)
     #Date modified
-    modified_date = models.DateTimeField(editable=False)
+    #modified_date = models.DateTimeField(editable=False)
     # Grant Status	Siebel	Active/Closed			GRANT_STATUS
     grant_status = models.CharField(max_length=50, default="Active",
                                     choices=(("Active", "Active"), ("Closed", "Closed")))
@@ -219,8 +219,8 @@ class ImportedGrant(models.Model):
     def get_diff_fields(self):
         model_fields = [field.name for field in self._meta.get_fields()]
         #imported_grants = self.grant.importedgrant_set.all()
-        date = self.modified_date
-        passed = self.grant.importedgrant_set.filter(modified_date__lt=date).order_by('modified_date')
+        date = self.creation_date
+        passed = self.grant.importedgrant_set.filter(creation_date__lt=date).order_by('creation_date')
         if len(list(passed)) > 0:
             previous = list(passed)[-1]
             if previous:
@@ -234,7 +234,7 @@ class ImportedGrant(models.Model):
         exists = Grant.objects.filter(grant_ref=self.grant_ref).exists()
         if not self.creation_date:
             self.creation_date = timezone.now()
-        self.modified_date = timezone.now()
+        #self.modified_date = timezone.now()
 
         if exists:
             existing_grant = Grant.objects.get(grant_ref=self.grant_ref)
