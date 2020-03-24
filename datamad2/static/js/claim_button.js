cda49c1b3567e7222ff6ee72c6bb2cf6ebec6437
ellesmith88 <e.s.smith@hotmail.co.uk>
2020-03-24 14:29:20 +0000

    increased spacing between rows grant detail

--- datamad2/static/js/claim_button.js
+++ datamad2/static/js/claim_button.js
@@ -21,27 +21,3 @@ $(".claim-btn").click(function () {
                 }
             });
         });
-
-        $(".claim-btn").click(function () {
-            let btn = $(this);
-            let url = '/' + 'grant/' + $(this).attr('data-id') + '/unclaim';
-            let cell = btn.parent();
-            let assign = cell[0].parentElement;
-            $.ajax({
-                type: "GET",
-                url: url,
-
-                // handle a successful response
-                success: function () {
-                    assign.innerHTML = "<td class=\"column-id\" id=\"right\">\n" +
-                        "                                        <a><button data-id=\"{{ imported_grant.grant.pk }}\" class=\"btn btn-primary claim-btn\"  id=\"claim-button\"> CLAIM</button></a>\n" +
-                        "                                        <a href=\"{% url 'change_claim' pk=imported_grant.grant.pk %}\"><button class=\"btn btn-primary\" id=\"claim\"> ASSIGN </button></a>\n" +
-                        "                                    </td>"
-                    cell.attr('id', 'claim');
-                },
-                // handle a non-successful response
-                error: function () {
-                    alert('Unclaim failed');
-                }
-            });
-        });
