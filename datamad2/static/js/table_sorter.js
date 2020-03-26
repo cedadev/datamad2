a5c90bfd74988f707e3f721bae5f00fc1e84c637
ellesmith88 <e.s.smith@hotmail.co.uk>
2020-03-26 09:29:56 +0000

    added state save to javascript data table

--- datamad2/static/js/table_sorter.js
+++ datamad2/static/js/table_sorter.js
@@ -1,35 +1,33 @@
 $(document).ready(function() {
     var table = $('#grants').DataTable( {
+        stateSave: true
     } );
 
     new $.fn.dataTable.FixedHeader( table );
 } );
 
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip();
 })
 
 
-$(".claim-btn").click(function () {
-    let btn = $(this);
-    let url = 'grant/' + $(this).attr('data-id') + '/claim';
-    let cell = btn.parent();
-    let data_centre = cell.parent()[0].cells[2];
-    console.log(data_centre)
-    $.ajax({
-        type: "GET",
-        url: url,
+$( ".claim-btn" ).click(function(){
+        let btn = $(this);
+        let url = 'grant/' + $(this).attr('data-id') + '/claim';
+        let cell = btn.parent();
 
-        // handle a successful response
-        success: function () {
-            cell.html("CLAIMED");
-            cell.attr('id', 'claim');
-            data_centre.innerHTML = "{{ user.data_centre }}"
-            console.log(cell.parent())
-        },
-        // handle a non-successful response
-        error: function () {
-            alert('Claim failed');
-        }
+        $.ajax({
+            type: "GET",
+            url: url,
+
+            // handle a successful response
+            success: function () {
+                cell.html("CLAIMED");
+                cell.attr('id', 'claim');
+            },
+            // handle a non-successful response
+            error: function () {
+                alert('Claim failed');
+            }
+        });
     });
-});
