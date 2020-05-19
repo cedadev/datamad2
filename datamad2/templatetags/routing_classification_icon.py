cbfa6fafacfd58dc47145835a51d609ca062737d
ellesmith88 <e.s.smith@hotmail.co.uk>
2020-05-19 13:09:22 +0100

    changes

--- datamad2/templatetags/routing_classification_icon.py
+++ datamad2/templatetags/routing_classification_icon.py
@@ -31,25 +31,10 @@ def icon(value):
                          "class='fa "
                          "fa-globe-americas fa-lg' style='color:#ba9b72'></i></a>")
 
-    elif value == "Earth Observation":
-        return mark_safe("<a id='science-icon' title='Earth Observation' data-toggle='tooltip'><i "
-                         "class='fas "
-                         "fa-satellite fa-lg' style='color:#9e9a8e'></i></a>")
-
-    elif value == "Panel A":
-        return mark_safe("<a id='science-icon' title='Panel A' data-toggle='tooltip'><i "
-                         "class='fab "
-                         "fa-adn fa-lg' ></i></a>")
-
-    elif value == "Panel B":
-        return mark_safe("<a id='science-icon' title='Panel B' data-toggle='tooltip'><i "
-                         "class='fas "
-                         "fa-bold fa-lg' ></i></a>")
-
-    elif value == "Panel C":
-        return mark_safe("<a id='science-icon' title='Panel C' data-toggle='tooltip'><i "
-                         "class='fas "
-                         "fa-copyright fa-lg' ></i></a>")
+    # elif value == "Earth Observation":
+    #     return mark_safe("<a id='science-icon' title='Earth Observation' data-toggle='tooltip'><i "
+    #                      "class='fas "
+    #                      "fa-satellite fa-lg' style='color:#9e9a8e'></i></a>")
 
     else:
         return mark_safe("<a id='science-icon' title='None' data-toggle='tooltip'><i class='fa "
