13b2928771578a0f5481b97d2322ed6275a60c91
rsmith013 <richard.d.smith@stfc.ac.uk>
2020-06-09 11:57:12 +0100

    Added clear selection button

--- datamad2/static/js/table_sorter.js
+++ datamad2/static/js/table_sorter.js
@@ -19,6 +19,44 @@ function display_hidden_facets(event, facet){
     }
 };
 
+$('.clear-facets').click(function (event) {
+    let facet = $(this).attr('data-facet')
+    let urlParams = new URLSearchParams(window.location.search)
+    let selectedFacets = urlParams.getAll('selected_facets')
+
+    // Search to see if the facet is in the list
+    let findex = -1;
+    for (i=0; i < selectedFacets.length;i++ ){
+        if (selectedFacets[i].startsWith(facet)){
+            findex=i;
+            break;
+        }
+    }
+
+    // Delete the facet
+    if (findex > -1){
+        selectedFacets.splice(findex, 1);
+    }
+
+    // Delete all selected facets
+    urlParams.delete('selected_facets')
+
+    // Re-add any remaining facets
+    selectedFacets.forEach(function (facet){
+        urlParams.append('selected_facets', facet)
+    })
+
+    // Handle adding the search character
+    let new_qs = urlParams.toString();
+    if (new_qs){
+        new_qs = '?'+new_qs
+    }
+
+    // Reload page with changes
+    window.location.href = window.location.origin + new_qs;
+
+    })
+
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 })
