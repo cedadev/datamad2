69e2e5297d189bb2e07ea590786152a7ab38564e
rsmith013 <richard.d.smith@stfc.ac.uk>
2020-06-11 17:32:45 +0100

    Minor formatting change

--- datamad2/tables.py
+++ datamad2/tables.py
@@ -14,10 +14,23 @@ from django_tables2.utils import A
 
 
 class GrantTable(tables.Table):
-    grant_ref = tables.LinkColumn(viewname='grant_detail', args=[A('importedgrant__pk')], verbose_name='Grant Reference')
-    routing_classification = tables.TemplateColumn(template_name='datamad2/fields/routing_classification_field.html')
-    date_added = tables.DateTimeColumn(accessor='importedgrant__creation_date', format='d M Y')
-    assigned_datacentre = tables.TemplateColumn(accessor='assigned_data_centre', template_name='datamad2/fields/assigned_datacentre_field.html', attrs={'td':{'class': 'align-middle text-center'}})
+    grant_ref = tables.LinkColumn(
+        viewname='grant_detail',
+        args=[A('importedgrant__pk')],
+        verbose_name='Grant Reference'
+    )
+    routing_classification = tables.TemplateColumn(
+        template_name='datamad2/fields/routing_classification_field.html'
+    )
+    date_added = tables.DateTimeColumn(
+        accessor='importedgrant__creation_date',
+        format='d M Y'
+    )
+    assigned_datacentre = tables.TemplateColumn(
+        accessor='assigned_data_centre',
+        template_name='datamad2/fields/assigned_datacentre_field.html',
+        attrs={'td':{'class': 'align-middle text-center'}}
+    )
 
     class Meta:
         model = Grant
