711a7bfd9d9ace0b4f9a9a44f292c42dce6c8482
rsmith013 <richard.d.smith@stfc.ac.uk>
2020-07-20 16:04:56 +0100

    Added version query params to custom javascript. Will allow us to modify this version with changes to files. This makes sure clients get the updates

--- setup.py
+++ setup.py
@@ -21,12 +21,12 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='1.0.0',
 
     description='datamad2',
 
     # The project's main homepage.
-    url='https://github.com/ellesmith88/datamad2',
+    url='https://github.com/cedadev/datamad2',
 
     # Author details
     author='Elle Smith',
