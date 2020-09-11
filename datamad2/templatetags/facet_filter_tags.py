afeef86013417e3a0fd7a7f344e978e7bcb53e33
rsmith013 <richard.d.smith@stfc.ac.uk>
2020-09-11 15:06:43 +0100

    Added selected filters at the top. closes #214

--- datamad2/templatetags/facet_filter_tags.py
+++ datamad2/templatetags/facet_filter_tags.py
@@ -11,6 +11,7 @@ __contact__ = 'richard.d.smith@stfc.ac.uk'
 from datamad2.search_indexes import ImportedGrantIndex
 from django import template
 from datamad2.utils import removesuffix
+from django.template.loader import get_template
 
 register = template.Library()
 
@@ -33,3 +34,19 @@ def available_facets(request):
             avail_facets.append(field)
 
     return [field for field in fields if field not in facet_preferences]
+
+
+@register.simple_tag(takes_context=True)
+def selected_facets(context):
+    request = context.request
+    selected_facets = request.GET.getlist('selected_facets')
+    converted_facets = {}
+
+    for facet in selected_facets:
+        f, label = facet.split(':')
+        title = f.replace('_', ' ').title()
+        converted_facets[title]=label
+
+    if converted_facets:
+        template = get_template('datamad2/includes/selected_facets.html')
+        return template.render({'converted_facets': converted_facets})
