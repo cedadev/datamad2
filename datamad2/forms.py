038250029f54283987c91183425fa2920e9207aa
rsmith013 <richard.d.smith@stfc.ac.uk>
2020-09-18 11:38:38 +0100

    WIP: Added form fields and filtering but the filters seem to reduce to 0 results. Need to look at the ES query to check that haystack is converting the query accurately and in a way compatible with es7.

--- datamad2/forms.py
+++ datamad2/forms.py
@@ -7,10 +7,10 @@ from datamad2.search_indexes import ImportedGrantIndex
 from crispy_forms.layout import Submit
 from .utils import removesuffix
 from datamad2.models.users import DataCentre
+from bootstrap_datepicker_plus import DatePickerInput
 
 
 class UpdateClaim(forms.ModelForm):
-
     class Meta:
         model = Grant
         fields = ('assigned_data_centre',)
@@ -21,7 +21,6 @@ class DateInput(forms.DateInput):
 
 
 class GrantInfoForm(forms.ModelForm):
-
     class Meta:
         model = Grant
         fields = (
@@ -53,14 +52,32 @@ class DatamadFacetedSearchForm(FacetedSearchForm):
         ('grant_holder_exact', 'Grant Holder (A-Z)'),
         ('-grant_holder_exact', 'Grant Holder (Z-A)'),
     )
-    sort_by = forms.ChoiceField(choices=CHOICES, required=False, widget=forms.Select(attrs={"onchange":"trigger_submit(this)"}))
+    sort_by = forms.ChoiceField(
+        choices=CHOICES,
+        required=False,
+        widget=forms.Select(attrs={"onchange": "trigger_submit(this)"})
+    )
+
+    start_date = forms.DateField(
+        required=False,
+        label='Start Date Beginning',
+        widget=DatePickerInput(options={
+            'format': 'DD/MM/YYYY'
+        })
+    )
+
+    end_date = forms.DateField(
+        required=False,
+        label='Start Date End',
+        widget=DatePickerInput(options={
+            'format': 'DD/MM/YYYY'
+        })
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.form_method='get'
-        self.helper.form_class='ml-0'
-        self.helper.form_id='search_form'
+        self.helper.form_tag = False
 
     def no_query_found(self):
         """
@@ -80,18 +97,22 @@ class DatamadFacetedSearchForm(FacetedSearchForm):
             order = self.cleaned_data['sort_by']
             sqs = sqs.order_by(order)
 
+        if self.cleaned_data['start_date']:
+            sqs = sqs.filter(acutal_start_date__gte=self.cleaned_data['start_date'])
+
+        if self.cleaned_data['end_date']:
+            sqs = sqs.filter(acutal_start_date__lte=self.cleaned_data['end_date'])
+
         return sqs
 
 
 class DocumentForm(forms.ModelForm):
-
     class Meta:
         model = Document
         fields = ('upload', 'tags')
 
 
 class MultipleDocumentUploadForm(forms.ModelForm):
-
     class Meta:
         model = Document
         fields = ('upload',)
@@ -115,7 +136,6 @@ class FacetPreferencesForm(forms.Form):
 
 
 class DatacentreForm(forms.ModelForm):
-
     class Meta:
         model = DataCentre
         fields = '__all__'
