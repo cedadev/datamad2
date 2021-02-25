8dbec3c01aea38ed523da278765f724c54b533b7
ellesmith88 <e.s.smith@hotmail.co.uk>
2021-02-25 15:02:47 +0000

    use intcomma template tag to add thousand comma separator

--- datamadsite/settings.py
+++ datamadsite/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -131,8 +132,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-USE_THOUSAND_SEPARATOR = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
