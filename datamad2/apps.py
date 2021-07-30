ce3a81389ec533a78e4e8933f6a113129da6cc84
Mahir-Sparkess <mahirrahman2000@gmail.com>
2021-07-30 14:43:51 +0100

    haystack-sizefields updated manually, all other packaged updated.

--- datamad2/apps.py
+++ datamad2/apps.py
@@ -3,6 +3,7 @@ from django.apps import AppConfig
 
 class Datamad2Config(AppConfig):
     name = 'datamad2'
+    default_auto_field = 'django.db.models.BigAutoField'
 
     def ready(self):
         import datamad2.signals
